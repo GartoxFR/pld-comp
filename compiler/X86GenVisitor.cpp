645ba9946c37ffcf4762925e058f72c3b1c22350
Ewan Chorynski <ewan71230@gmail.com>
2024-03-13 11:50:07 +0100

    Fix ASM Generator skeletton

--- compiler/X86GenVisitor.cpp
+++ compiler/X86GenVisitor.cpp
@@ -3,34 +3,16 @@
 
 using namespace ir;
 
-// helper type for the visitor
-template <class... Ts>
-struct overloaded : Ts... {
-    using Ts::operator()...;
-};
-// explicit deduction guide
-template <class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
+void X86GenVisitor::visit(ir::Function &function) {
+    //TODO: Generate asm
+}
+
+void X86GenVisitor::visit(ir::BasicBlock &block) {
+    //TODO: Generate asm
+}
 
 void X86GenVisitor::visit(ir::BinaryOp& binaryOp) {
     //TODO: Generate asm
-
-    RValue left = binaryOp.left();
-    RValue right = binaryOp.right();
-
-    std::visit(
-        overloaded{
-            [&](Local& left, Immediate& right) {
-                // Left member is a local variable and right is an immediate
-            },
-            [&](Immediate& left, Local& right) {
-
-            },
-            [&](Local& right, Local& left) {},
-            [&](Immediate& right, Immediate& left) {},
-        },
-        left, right
-    );
 }
 
 void X86GenVisitor::visit(ir::Assignment& assignment) {
@@ -38,5 +20,8 @@ void X86GenVisitor::visit(ir::Assignment& assignment) {
 }
 
 void X86GenVisitor::visit(ir::BasicJump& jump) {
+    //TODO: Generate asm
+}
+void X86GenVisitor::visit(ir::ConditionalJump& jump) {
     //TODO: Generate asm
 }
--- compiler/X86GenVisitor.h
+++ compiler/X86GenVisitor.h
@@ -4,7 +4,10 @@
 
 class X86GenVisitor : public ir::Visitor {
   public:
+    void visit(ir::Function &function) override;
+    void visit(ir::BasicBlock &block) override;
     void visit(ir::BinaryOp& binaryOp) override;
     void visit(ir::Assignment& assignment) override;
     void visit(ir::BasicJump& jump) override;
+    void visit(ir::ConditionalJump& jump) override;
 };
